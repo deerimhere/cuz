ed4188307e7225047964c6b02be0bc56a3a32192
deerimhere <cuzmeyou@gmail.com>
2024-07-08 18:37:57 +0900

    가상정원거의최종본

--- lib/models/tree_model.dart
+++ lib/models/tree_model.dart
@@ -6,38 +6,55 @@ class Tree {
   int level;
   List<bool> evolutionStages;
 
-  Tree({this.experience = 0, this.level = 1, List<bool>? evolutionStages})
-      : evolutionStages = evolutionStages ?? [false, false, false, false];
+  Tree({this.experience = 0, this.level = 0, List<bool>? evolutionStages})
+      : evolutionStages = evolutionStages ?? [false, false, false, false, false, false];
 
   void addExperience(int exp) {
     experience += exp;
-    if (experience >= 10000) {
-      experience = 10000;
+    if (experience >= 3500) {
+      experience = 3500;
     }
   }
 
   void evolve() {
-    if (experience >= level * 2500 && level < 4) {
+    if (experience >= (level + 1) * 500 && level < 7) {
       level++;
     }
   }
 
   void evolveWithItem() {
-    if (level < 4) {
-      if (experience >= 2500 && !evolutionStages[0]) {
+    if (level < 7) {
+      if (experience >= 500 && !evolutionStages[0]) {
         level++;
         evolutionStages[0] = true;
-      } else if (experience >= 5000 && !evolutionStages[1]) {
+      } else if (experience >= 1000 && !evolutionStages[1]) {
         level++;
         evolutionStages[1] = true;
-      } else if (experience >= 7500 && !evolutionStages[2]) {
+      } else if (experience >= 1500 && !evolutionStages[2]) {
         level++;
         evolutionStages[2] = true;
-      } else if (experience >= 10000 && !evolutionStages[3]) {
+      } else if (experience >= 2000 && !evolutionStages[3]) {
         level++;
         evolutionStages[3] = true;
+      } else if (experience >= 2500 && !evolutionStages[4]) {
+        level++;
+        evolutionStages[4] = true;
+      } else if (experience >= 3000 && !evolutionStages[5]) {
+        level++;
+        evolutionStages[5] = true;
       }
     }
+  }
+
+  void reset() {
+    experience = 0;
+    level = 0;
+    evolutionStages = [false, false, false, false, false, false];
+  }
+
+  void plantSeed() {
+    level = 1;
+    experience = 0;
   }
 }
 
@@ -49,12 +66,14 @@ class TreeManager extends ChangeNotifier {
   Future<void> loadTree() async {
     final prefs = await SharedPreferences.getInstance();
     _tree.experience = prefs.getInt('treeExperience') ?? 0;
-    _tree.level = prefs.getInt('treeLevel') ?? 1;
+    _tree.level = prefs.getInt('treeLevel') ?? 0;
     _tree.evolutionStages = [
       prefs.getBool('evolutionStage0') ?? false,
       prefs.getBool('evolutionStage1') ?? false,
       prefs.getBool('evolutionStage2') ?? false,
       prefs.getBool('evolutionStage3') ?? false,
+      prefs.getBool('evolutionStage4') ?? false,
+      prefs.getBool('evolutionStage5') ?? false,
     ];
     notifyListeners();
   }
@@ -67,6 +86,8 @@ class TreeManager extends ChangeNotifier {
     await prefs.setBool('evolutionStage1', _tree.evolutionStages[1]);
     await prefs.setBool('evolutionStage2', _tree.evolutionStages[2]);
     await prefs.setBool('evolutionStage3', _tree.evolutionStages[3]);
+    await prefs.setBool('evolutionStage4', _tree.evolutionStages[4]);
+    await prefs.setBool('evolutionStage5', _tree.evolutionStages[5]);
   }
 
   void addExperience(int exp) {
@@ -83,6 +104,18 @@ class TreeManager extends ChangeNotifier {
 
   void evolveWithItem() {
     _tree.evolveWithItem();
+    saveTree();
+    notifyListeners();
+  }
+
+  void resetTree() {
+    _tree.reset();
+    saveTree();
+    notifyListeners();
+  }
+
+  void plantSeed() {
+    _tree.plantSeed();
     saveTree();
     notifyListeners();
   }
--- lib/pages/garden_page.dart
+++ lib/pages/garden_page.dart
@@ -18,28 +18,54 @@ class GardenPage extends StatelessWidget {
           children: [
             Expanded(
               child: Center(
-                child: Consumer<TreeManager>(
-                  builder: (context, treeManager, child) {
+                child: Consumer2<TreeManager, ItemData>(
+                  builder: (context, treeManager, itemData, child) {
+                    final evolveItem = itemData.items.firstWhere((item) => item.name == '진화!');
                     return Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text('나무 레벨: ${treeManager.tree.level}', style: TextStyle(fontSize: 24)),
-                        SizedBox(height: 20),
-                        Text('경험치: ${treeManager.tree.experience} / 10000', style: TextStyle(fontSize: 18)),
-                        SizedBox(height: 20),
-                        LinearProgressIndicator(
-                          value: treeManager.tree.experience / 10000,
-                          minHeight: 20,
-                        ),
+                        if (treeManager.tree.level == 0)
+                          Text(
+                            '씨앗을 심어주세요!',
+                            style: TextStyle(fontSize: 24),
+                          )
+                        else ...[
+                          Text('나무 레벨: ${treeManager.tree.level}', style: TextStyle(fontSize: 24)),
+                          SizedBox(height: 20),
+                          Image.asset(
+                            'assets/images/level${treeManager.tree.level}.png',
+                            height: 200,
+                            width: 200,
+                          ),
+                          SizedBox(height: 20),
+                          Text('경험치: ${treeManager.tree.experience} / 3500', style: TextStyle(fontSize: 18)),
+                          SizedBox(height: 20),
+                          Container(
+                            width: 300, // 경험치바의 길이를 조절합니다.
+                            child: LinearProgressIndicator(
+                              value: treeManager.tree.experience / 3500,
+                              minHeight: 20,
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: treeManager.tree.level < 7 &&
+                                    treeManager.tree.experience >= (treeManager.tree.level * 500) &&
+                                    evolveItem.quantity > 0
+                                ? () {
+                                    treeManager.evolve();
+                                    itemData.useItem('진화!');
+                                  }
+                                : null,
+                            child: Text('진화하기'),
+                          ),
+                        ],
                         SizedBox(height: 20),
                         ElevatedButton(
-                          onPressed: treeManager.tree.level < 4 &&
-                                  treeManager.tree.experience >= treeManager.tree.level * 2500
-                              ? () {
-                                  treeManager.evolve();
-                                }
-                              : null,
-                          child: Text('진화하기'),
+                          onPressed: () {
+                            treeManager.resetTree();
+                          },
+                          child: Text('초기화하기'),
                         ),
                       ],
                     );
@@ -59,7 +85,7 @@ class GardenPage extends StatelessWidget {
                       return Wrap(
                         spacing: 10,
                         children: [
-                          _buildItemButton(context, itemData, '씨앗', Icons.grass, 0),
+                          _buildItemButton(context, itemData, '씨앗', Icons.grass, 0, isSeed: true),
                           _buildItemButton(context, itemData, '물', Icons.water_drop, 10),
                           _buildItemButton(context, itemData, '비료', Icons.eco, 20),
                           _buildItemButton(context, itemData, '영양제', Icons.local_florist, 50),
@@ -78,7 +104,7 @@ class GardenPage extends StatelessWidget {
   }
 
   Widget _buildItemButton(BuildContext context, ItemData itemData, String itemName, IconData icon, int exp,
-      {bool isEvolve = false}) {
+      {bool isEvolve = false, bool isSeed = false}) {
     final treeManager = Provider.of<TreeManager>(context, listen: false);
     final item = itemData.items.firstWhere((item) => item.name == itemName);
     return Column(
@@ -87,7 +113,9 @@ class GardenPage extends StatelessWidget {
           icon: Icon(icon, size: 40),
           onPressed: item.quantity > 0
               ? () {
-                  if (isEvolve) {
+                  if (isSeed) {
+                    treeManager.plantSeed();
+                  } else if (isEvolve) {
                     treeManager.evolveWithItem();
                   } else {
                     treeManager.addExperience(exp);
